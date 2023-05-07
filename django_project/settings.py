5df7eccf3abceb6b45024486085c2f7d4e7c3892
Carlos Martinelli <esc4dinh4@hotmail.com>
2023-05-07 18:39:48 -0400

    deploy static

--- django_project/settings.py
+++ django_project/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR.joinpath("media")
 # STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
