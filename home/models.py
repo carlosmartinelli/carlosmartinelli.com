6798f4a2c496a1a19dca8d000c8204873ed43312
Carlos Martinelli <esc4dinh4@hotmail.com>
2023-06-09 23:56:37 -0400

    adding a railway config

--- home/models.py
+++ home/models.py
@@ -1,15 +1,32 @@
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField, StreamField
+from wagtail.fields import StreamField
+# from wagtail.admin.panels import FieldPanel, StreamFieldPanel
+# from wagtail.images.blocks import ImageChooserBlock
+# from streams import blocks.BodyBlock
+
+
+from streams import blocks
+
 
 
 class HomePage(Page):
     template = "home/index.html"
 
-    body = RichTextField(blank=True)
+    body = StreamField(BodyBlock(), blank=True)
+
+
+    # body = RichTextField(blank=True)
+
+    # body = StreamField([
+    #     ('heading', blocks.CharBlock(form_classname="title")),
+    #     ('paragraph', blocks.RichTextBlock()),
+    #     ('image', ImageChooserBlock()),
+    # ], use_json_field=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('body'),
+        StreamFieldPanel('content'),
     ]
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+##
+
+## The Following command is for run the tailwind css and watch changes
+`npx tailwindcss -i ./static/src/scss/main.scss -o ./static/css/main.css --watch`
+
